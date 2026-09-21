44885797bcc947d27562a1c16d2f53c2f81a3673
Adya Neetha Lakshmi P B <adyaneeth123@gmail.com>
2026-09-21 17:35:41 +0530

    Create input_my little plant.c

---
+++ input_my little plant.c
@@ -0,0 +1,80 @@
+================================
+MY LITTLE PLANT
+
+Enter plant name: Ava
+
+================================
+Day 1 | Ava
+
+Energy: 50  Happiness: 50
+Health: 50   Score: 0
+
+1. Water
+
+
+2. Sunlight
+
+
+3. Feed
+
+
+4. Play
+
+
+5. Check Status
+
+
+6. Exit
+
+
+
+Enter choice: 2
+
+Ava enjoyed the sunlight!
+
+What do you want to do?
+
+1. Go to Day 2
+
+
+2. Play Day 1 again
+
+
+3. Exit
+
+
+
+Enter choice: 2
+
+Playing Day 1 again...
+
+================================
+Day 1 | Ava
+
+Energy: 60  Happiness: 55
+Health: 55   Score: 5
+
+1. Water
+
+
+2. Sunlight
+
+
+3. Feed
+
+
+4. Play
+
+
+5. Check Status
+
+
+6. Exit
+
+
+
+Enter choice: 6
+
+Goodbye, Ava!
+Final Score: 5
+Can you make it as a pdf group4 input.c
